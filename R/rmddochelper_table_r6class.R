9fcafddfa5638aad30112cca276fc9e1be49a1ec
agi0917 <peter.vonrohr@gmail.com>
2016-07-26 11:41:58 +0200

    Updated management of document status

--- R/rmddochelper_table_r6class.R
+++ R/rmddochelper_table_r6class.R
@@ -27,88 +27,81 @@
 #' @format \code{\link{R6Class}} object.
 #' @examples
 #' r6objDocuStat <- R6ClassDocuStatus$new()
-#' r6objDocuStat$setProject(psProject = "DEXSeq")
-#' r6objDocuStat$setVersion(psVersion = "0.0.900")
-#' r6objDocuStat$setDate(psDate = "31.03.2016")
-#' r6objDocuStat$setAuthor(psAuthor = "pvr")
-#' r6objDocuStat$setStatus(psStatus = "Init")
+#' r6obj_docstat$set_current_status(psVersion = "0.0.908",
+#'                                  psStatus  = "New version of setting document status",
+#'                                  psProject = "rmddochelper")
 #' \dontrun{
 #' r6objDocuStat$writeStatusToFile()
 #' r6objDocuStat$knitr_kable()
 #' }
 #' @field version current version of the document
-#' @field author author of the change leading to the current status of the document
-#' @field date date of the current change
-#' @field status description of document status
-#' @field project project this document belongs to
 #' @field status_colnames vector of column names shown in the document table
 #' @field status_history dataframe with the document history read from the history file
 #' @field history_file name of the file containing the document history
 #' @section Public Methods:
 #' \describe{
 #'   \item{\code{new()}}{This method instantiates an object of class R6ClassDocuStatus}
-#'   \item{\code{initialize()}}{Initialization of field called after creating the instance}
+#'   \item{\code{initialize(psFormat)}}{Initialization after creating the instance. Reads history file if it exists}
 #'   \item{\code{include_doc_stat(psTitle)}}{Saves updated document status to the status file.
 #'               Write section header psTitle for document status and write
 #'               markdown table containing the document status.}
 #' }
 #' @section Private Methods:
 #' \describe{
-#'   \item{\code{writeStatusToFile(psFileName = NULL)}}{Writes current status and history
+#'   \item{\code{writeStatusToFile()}}{Writes current status and history
 #'               to a tab-separated file. Tab-separated format is chosen, because TAB-characters
-#'               are less likely to occur in any of the table fields. If argument
-#'               psFileName is not null the name of the output file is set to psFileName,
-#'               otherwise the value in field history_file is used. File encoding is set
+#'               are less likely to occur in any of the table fields. File encoding is set
 #'               to "UTF-8" in order to preserve German Umlauts}
-#'   \item{\code{readStatusFromFile(psFileName = NULL)}}{Document status history is read
-#'               from the history file. The name of the
-#'               history file is either taken from the method argument psFileName or from
-#'               the object field history_file.}
-#'   \item{\code{readCsv2StatusFromFile(psFileName = NULL)}}{Reading method for old csv2
+#'   \item{\code{readStatusFromFile()}}{Document status history is read
+#'               from the history file.}
+#'   \item{\code{readCsv2StatusFromFile()}}{Reading method for old csv2
 #'               formatted status history files. This is mainly used for converting history
 #'               files from old csv2 format to new tab-separated format.}
-#'   \item{\code{knitr_kable}}{Add current document status info to document history and
-#'               convert it to a dataframe. Then use \code{knitr::kable} to convert the
-#'               dataframe into a markdown-table}
-#'   \item{\code{stat_to_df}}{}
+#'   \item{\code{knitr_kable}}{Complete status history is written to a table using
+#'         \code{knitr::kable} to convert the dataframe into a markdown-table}
+#'   \item{\code{auto_increment()}}{Autoincrementing minor version number}
 #' }
 R6ClassDocuStatus <- R6::R6Class(classname = "R6ClassDocuStatus",
                                  public    = list(
-                                   initialize = function(){
+                                   initialize = function(psFormat = "tab") {
                                      'Initialisation of a new document status object.'
-                                     ### # initialize date and author based on Sys functionis
-                                     self$setDate(psDate = as.character(Sys.Date()))
-                                     self$setAuthor(psAuthor = Sys.info()[["user"]])
+                                     if (psFormat == "csv2"){
+                                       private$readCsv2StatusFromFile()
+                                     } else {
+                                       private$readStatusFromFile()
+                                     }
                                    },
-                                   setVersion = function(psVersion){
-                                     private$version <- psVersion
-                                   },
-                                   getVersion = function(){
-                                     return(private$version)
-                                   },
-                                   setDate = function(psDate){
-                                     private$date <- psDate
-                                   },
-                                   getDate = function(){
-                                     return(private$date)
-                                   },
-                                   setAuthor = function(psAuthor){
-                                     private$author <- psAuthor
-                                   },
-                                   getAuthor = function(){
-                                     return(private$author)
-                                   },
-                                   setStatus = function(psStatus){
-                                     private$status = psStatus
-                                   },
-                                   getStatus = function(){
-                                     return(private$status)
-                                   },
-                                   setProject = function(psProject){
-                                     private$project <- psProject
-                                   },
-                                   getProject = function(){
-                                     return(private$project)
+                                   set_current_status = function(psVersion = private$auto_increment(),
+                                                                 psDate = as.character(Sys.Date()),
+                                                                 psAuthor = Sys.info()[["user"]],
+                                                                 psStatus = NULL,
+                                                                 psProject = "NA"){
+                                     ### # in case status is not null, add status, o/w do nothing
+                                     if (!is.null(psStatus)) {
+                                       dfCurStatus <- data.frame(Version = psVersion,
+                                                                 Date = psDate,
+                                                                 Author = psAuthor,
+                                                                 Status = psStatus,
+                                                                 Project = psProject,
+                                                                 stringsAsFactors = FALSE)
+                                       if (is.null(private$status_history)){
+                                         private$status_history <- dfCurStatus
+                                       } else {
+                                         ### # in case psVersion already exists in private$status_history$Version,
+                                         ### #  update other fields
+                                         if (is.element(psVersion, private$status_history$Version)){
+                                           nUpdateRow <- which(psVersion == private$status_history$Version)
+                                           private$status_history[nUpdateRow,] <- dfCurStatus[1,]
+                                         } else {
+                                           private$status_history <- rbind(private$status_history,
+                                                                           dfCurStatus,
+                                                                           stringsAsFactors = FALSE)
+                                         }
+
+                                       }
+
+                                     }
+
                                    },
                                    setStatusColnames = function(psStatusColnames){
                                      private$status_colnames <- psStatusColnames
@@ -119,32 +112,22 @@ R6ClassDocuStatus <- R6::R6Class(classname = "R6ClassDocuStatus",
                                    setTitle = function(pstitle){
                                      private$stitle  <- pstitle
                                    },
-                                   include_doc_stat = function(psTitle = NULL, psFormat = "tab"){
-                                     ### # read status history, if it exists
-                                     if (file.exists(private$history_file)){
-                                       if (psFormat == "csv2"){
-                                         private$readCsv2StatusFromFile()
-                                       } else {
-                                         private$readStatusFromFile()
-                                       }
-                                     }
-                                     ### # write current status plus history to file
+                                   include_doc_stat = function(psTitle = NULL){
+                                     ### # write complete status to file
                                      private$writeStatusToFile()
-                                     ### # get the title
+                                     ### # get the title in case it got changed
                                      if (!is.null(psTitle)){
                                        stitle <- psTitle
                                      } else {
                                        stitle <- private$stitle
                                      }
+                                     ### # write title to document
                                      cat(stitle,"\n", sep = "")
+                                     ### # convert status dataframe to a table
                                      private$knitr_kable()
                                    }
                                  ),
                                  private   = list(version         = "0.0.900",
-                                                  author          = NULL,
-                                                  date            = NULL,
-                                                  status          = "Init",
-                                                  project         = "NA",
                                                   status_colnames = c("Version", "Date", "Author","Status","Project"),
                                                   status_history  = NULL,
                                                   history_file    = "DOCUMENTSTATUS",
@@ -152,41 +135,29 @@ R6ClassDocuStatus <- R6::R6Class(classname = "R6ClassDocuStatus",
                                                   get_version_col = function(pdfStatus){
                                                     return(which(tolower(names(pdfStatus)) == "version"))
                                                   },
-                                                  stat_to_df = function() {
-                                                    dfCurStatus <- data.frame(version = private$version,
-                                                                              date    = private$date,
-                                                                              author  = private$author,
-                                                                              status  = private$status,
-                                                                              project = private$project,
-                                                                              stringsAsFactors = FALSE)
-                                                    if (!is.null(private$status_history)){
-                                                      ### # adaptation of names, o/w rbind will not work
-                                                      names(dfCurStatus) <- names(private$status_history)
-                                                      ### # check whether version number already exists
-                                                      version_col_idx <- private$get_version_col(pdfStatus = dfCurStatus)
-                                                      cur_row_col <- which(dfCurStatus[,version_col_idx] == private$status_history[,version_col_idx])
-                                                      if (length(cur_row_col) > 0) {
-                                                        dfCurStatus <- rbind(private$status_history[-cur_row_col,], dfCurStatus)
-                                                      } else {
-                                                        dfCurStatus <- rbind(private$status_history, dfCurStatus)
-                                                      }
+                                                  auto_increment = function(){
+                                                    if (is.null(private$status_history)){
+                                                      sCurVersion <- private$version
+                                                    } else {
+                                                      sCurVersion <- private$status_history[nrow(private$status_history),"Version"]
                                                     }
-                                                    return(dfCurStatus)
+                                                    vVersionComp <- as.numeric(unlist(strsplit(sCurVersion,
+                                                                                    split = ".",
+                                                                                    fixed = TRUE)))
+                                                    vVersionComp[3] <- vVersionComp[3] + 1
+                                                    return(paste(as.character(vVersionComp), collapse = "."))
                                                   },
-                                                  writeStatusToFile = function(psFileName = NULL){
-                                                    ### # convert status to a dataframe and write it to the history file
-                                                    dfCurStatus <- private$stat_to_df()
-                                                    names(dfCurStatus) <- private$status_colnames
-                                                    write.table(dfCurStatus, file = private$history_file,
+                                                  writeStatusToFile = function(){
+                                                    ### # write complete status history to history file
+                                                    write.table(private$status_history,
+                                                                file = private$history_file,
                                                                 quote = FALSE,
                                                                 sep = "\t",
                                                                 row.names = FALSE,
                                                                 fileEncoding = "UTF-8")
                                                   },
-                                                  readStatusFromFile = function(psFileName = NULL){
-                                                    sFileName <- psFileName
-                                                    if (is.null(psFileName))
-                                                      sFileName <- private$history_file
+                                                  readStatusFromFile = function(){
+                                                    sFileName <- private$history_file
                                                     if (!file.exists(sFileName))
                                                       stop("CANNOT FIND Status file: ", sFileName)
                                                     private$status_history <- read.table(file = sFileName,
@@ -196,10 +167,8 @@ R6ClassDocuStatus <- R6::R6Class(classname = "R6ClassDocuStatus",
                                                                                          stringsAsFactors = FALSE,
                                                                                          fileEncoding = "UTF-8")
                                                   },
-                                                  readCsv2StatusFromFile = function(psFileName = NULL){
-                                                    sFileName <- psFileName
-                                                    if (is.null(psFileName))
-                                                      sFileName <- private$history_file
+                                                  readCsv2StatusFromFile = function(){
+                                                    sFileName <- private$history_file
                                                     if (!file.exists(sFileName))
                                                       stop("CANNOT FIND Status file: ", sFileName)
                                                     private$status_history <- read.csv2(file = sFileName,
@@ -208,8 +177,10 @@ R6ClassDocuStatus <- R6::R6Class(classname = "R6ClassDocuStatus",
                                                                                         fileEncoding = "UTF-8")
                                                   },
                                                   knitr_kable = function(){
-                                                    dfCurStatus <- private$stat_to_df()
-                                                    knitr::kable(dfCurStatus)
+                                                    ### # in case different column titles are specified, we change it
+                                                    dfDocStatus <- private$status_history
+                                                    names(dfDocStatus) <- private$status_colnames
+                                                    knitr::kable(dfDocStatus)
                                                   }))
 
 
